70294a908eb7d9e9523fa0eb22a951921d60b336
Gabriel Sampaio <gsampaio@redhat.com>
2023-10-06 14:03:15 -0300

    movelog

--- src/main/java/com/redhat/quarkus/service/AccessLogService.java
+++ src/main/java/com/redhat/quarkus/service/AccessLogService.java
@@ -1,23 +1,23 @@
 package com.redhat.quarkus.service;
 
 import com.redhat.quarkus.model.AccessLog;
+import com.redhat.quarkus.model.MoveLog;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import java.util.List;
-
 
 @ApplicationScoped
 public class AccessLogService {
 
-
     @Incoming("lobby")
     @Outgoing("entrance")
     @Blocking
-    public AccessLog processLobbyEvent(AccessLog accessLog) throws InterruptedException {
-        return accessLog;
+    public MoveLog processLobbyEvent(AccessLog accessLog) throws InterruptedException {
+        MoveLog moveLog = new MoveLog();
+        moveLog.setDestination(accessLog.getDestination());
+        moveLog.setPersonId(accessLog.getPersonId());
+        return moveLog;
     }
 
 }
